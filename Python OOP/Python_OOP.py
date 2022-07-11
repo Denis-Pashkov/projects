1989f89ef93af8681f9756b06ad21f5abc6b36ed
Denis <den.pashkov.98@gmail.com>
2022-07-12 02:35:48 +0300

    Python_OOP

--- Python OOP/Python_OOP.py
+++ Python OOP/Python_OOP.py
@@ -2199,3 +2199,41 @@ class Papyri(CommonAttr):
     def __init__(self, name: str, date: str, descr: str) -> None:
         super().__init__(name, descr)
         self.date = date
+        
+
+class SmartPhone:
+    def __init__(self, model):
+        self.model = model
+        self.apps = []
+
+    def add_app(self, app):
+        if len([i for i in self.apps if type(i) == type(app)]) == 0:
+            self.apps.append(app)
+
+    def remove_app(self, app):
+        self.apps.pop(self.apps.index(app))
+
+
+class AppVK:
+    def __init__(self):
+        self.name = 'ВКонтакте'
+
+
+class AppYouTube:
+    def __init__(self, memory_max):
+        self.name = 'YouTube'
+        self.memory_max = memory_max
+
+
+class AppPhone:
+    def __init__(self, phone_list):
+        self.name = 'Phone'
+        self.phone_list = phone_list
+
+
+sm = SmartPhone("Honor 1.0")
+sm.add_app(AppVK())
+sm.add_app(AppVK())  # второй раз добавляться не должно
+sm.add_app(AppYouTube(2048))
+for a in sm.apps:
+    print(a.name)
