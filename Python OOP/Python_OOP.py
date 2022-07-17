bfd6242c467a2f9e858749b17ff088c8df7d7f3b
Denis <den.pashkov.98@gmail.com>
2022-07-17 14:00:05 +0300

    Python_OOP

--- Python OOP/Python_OOP.py
+++ Python OOP/Python_OOP.py
@@ -2374,3 +2374,51 @@ filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.jpg", "forest
 acceptor = ImageFileAcceptor(('jpg', 'bmp', 'jpeg'))
 image_filenames = filter(lambda x: acceptor(x), filenames)
 print(list(image_filenames))
+
+
+from string import ascii_lowercase, digits
+
+
+class LoginForm:
+    def __init__(self, name, validators=None):
+        self.name = name
+        self.validators = validators
+        self.login = ""
+        self.password = ""
+
+    def post(self, request):
+        self.login = request.get('login', "")
+        self.password = request.get('password', "")
+
+    def is_validate(self):
+        if not self.validators:
+            return True
+
+        for v in self.validators:
+            if not v(self.login) or not v(self.password):
+                return False
+
+        return True
+
+
+class LengthValidator:
+    def __init__(self, min_length: int, max_length: int) -> None:
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        return bool(self.min_length < len(args[0]) < self.max_length)
+
+
+class CharsValidator:
+    def __init__(self, chars: str) -> None:
+        self.chars = chars
+
+    def __call__(self, *args, **kwargs):
+        return bool(len(args[0].strip(self.chars)) == 0)
+
+
+lg = LoginForm("Вход на сайт", validators=[LengthValidator(3, 50), CharsValidator(ascii_lowercase + digits)])
+lg.post({"login": "root", "password": "panda"})
+if lg.is_validate():
+    print("Дальнейшая обработка данных формы")
